93ff225c7c731532f3ac92bb2eeca90a281f5479
Michael Xi <ximichael26@gmail.com>
2025-05-09 19:15:33 -0700

    more bugs

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Enemy
+{
+    public string name;
+    public int sprite;
+    public int hp;
+    public int speed;
+    public int damage;
+}
