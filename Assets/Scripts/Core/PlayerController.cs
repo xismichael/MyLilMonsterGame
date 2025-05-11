cf9ea176a946ac4edc9e603f520f0368d31085ab
Michael Xi <ximichael26@gmail.com>
2025-05-11 14:13:43 -0700

    fixes

---
+++ Assets/Scripts/Core/PlayerController.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    public Hittable hp;
+    public HealthBar healthui;
+    public ManaBar manaui;
+
+    public SpellCaster spellcaster;
+    public SpellUIContainer spellUIContainer;
+
+    public int speed;
+    public Unit unit;
+
+    public string role;
+
+    private RoleClass currentRole;
+
+    public int lastMoveTime;
+    public event Action<float> OnStandStill;
+
+    void Start()
+    {
+        unit = GetComponent<Unit>();
+        GameManager.Instance.player = gameObject;
+        role = "mage";
+    }
+
+    public void StartLevel()
+    {
+        spellcaster = new SpellCaster(125, 8, Hittable.Team.PLAYER);
+        StartCoroutine(spellcaster.ManaRegeneration());
+
+        hp = new Hittable(100, Hittable.Team.PLAYER, gameObject);
+        hp.OnDeath += Die;
+        hp.team = Hittable.Team.PLAYER;
+
+        healthui.SetHealth(hp);
+        manaui.SetSpellCaster(spellcaster);
+        spellUIContainer.LoadUI(spellcaster.spells);
+        currentRole = RoleClassDatabase.Instance.GetRoleClass(role);
+        setStats(currentRole);
+        GameManager.Instance.OnWaveEnd += () => setStats(currentRole);
+    }
+
+    void Update()
+    {
+        // Movement handled by OnMove
+        OnStandStill?.Invoke(Time.time - lastMoveTime);
+    }
+
+    void setStats(RoleClass role)
+    {
+        spellcaster.mana = Mathf.RoundToInt(RPNEvaluator.Evaluate(role.mana, GetRPNVariables()));
+        spellcaster.mana_reg = Mathf.RoundToInt(RPNEvaluator.Evaluate(role.mana_regeneration, GetRPNVariables()));
+        spellcaster.power = Mathf.RoundToInt(RPNEvaluator.Evaluate(role.spellpower, GetRPNVariables()));
+        hp.SetMaxHP(Mathf.RoundToInt(RPNEvaluator.Evaluate(role.health, GetRPNVariables())));
+        speed = Mathf.RoundToInt(RPNEvaluator.Evaluate(role.speed, GetRPNVariables()));
+    }
+
+    public Dictionary<string, float> GetRPNVariables()
+    {
+        return new Dictionary<string, float>
+        {
+            { "wave", EnemySpawner.CurrentWaveNumber }
+        };
+    }
+
+    void OnAttack(InputValue value)
+    {
+        if (GameManager.Instance.state == GameManager.GameState.PREGAME ||
+            GameManager.Instance.state == GameManager.GameState.WAVEEND ||
+            GameManager.Instance.state == GameManager.GameState.GAMEOVER) return;
+
+        Vector2 mouseScreen = Mouse.current.position.value;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
+        mouseWorld.z = 0;
+
+        StartCoroutine(spellcaster.Cast(transform.position, mouseWorld));
+    }
+
+    void OnMove(InputValue value)
+    {
+        if (GameManager.Instance.state == GameManager.GameState.PREGAME ||
+            GameManager.Instance.state == GameManager.GameState.WAVEEND ||
+            GameManager.Instance.state == GameManager.GameState.GAMEOVER) return;
+
+        lastMoveTime = Time.time;
+        unit.movement = value.Get<Vector2>() * speed;
+    }
+
+    void OnChangeSpell(InputValue value)
+    {
+        spellUIContainer.spellUIs[spellcaster.spellCastIndex].GetComponent<SpellUI>().highlight.SetActive(false);
+        spellcaster.spellCastIndex = (spellcaster.spellCastIndex + 1) % spellcaster.GetCurrentSpellAmount();
+    }
+
+    void Die()
+    {
+        GameManager.Instance.state = GameManager.GameState.GAMEOVER;
+        GameManager.Instance.PlayerDeath = true;
+        EnemySpawner.Instance.StopAllCoroutines();
+
+        var allEnemies = FindObjectsOfType<EnemyController>();
+        foreach (var ec in allEnemies)
+        {
+            GameManager.Instance.RemoveEnemy(ec.gameObject);
+            Destroy(ec.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Movement/EnemyController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    public int speed;
+    public int damage = 5;
+    public Hittable hp;
+    public HealthBar healthui;
+    public bool dead;
+
+    public float last_attack;
+
+    public static Action OnAllEnemyDeath;
+    public Action OnEnemyDeath;
+
+    // Start is called once before the first execution of update after the monoBehaviour is created
+    void Start()
+    {
+        target = GameManager.Instance.player.transform;
+        hp.OnDeath += Die;
+        healthui.SetHealth(hp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        if (direction.magnitude < 2f)
+        {
+            DoAttack();
+        }
+        else
+        {
+            GetComponent<Unit>().movement = direction.normalized * speed;
+        }
+    }
+
+    void DoAttack()
+    {
+        if (last_attack + 2 < Time.time)
+        {
+            last_attack = Time.time;
+            target.gameObject.GetComponent<PlayerController>().hp.Damage(new Damage(damage, Damage.Type.PHYSICAL));
+        }
+    }
+
+    void Die()
+    {
+        if (!dead)
+        {
+            dead = true;
+
+            //counts enemy kill this wave
+            EnemySpawner.Instance.currentWaveEnemiesKilled++;
+            EnemySpawner.Instance.TotalEnemiesKilled++;
+
+            GameManager.Instance.RemoveEnemy(gameObject);
+            Destroy(gameObject);
+            OnAllEnemyDeath?.Invoke();
+            OnEnemyDeath?.Invoke();
+        }
+    }
+}
