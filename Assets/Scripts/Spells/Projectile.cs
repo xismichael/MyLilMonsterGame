2db9286dd74a88d264cd166371d575d8d4c365f2
Michael Xi <ximichael26@gmail.com>
2025-06-03 16:48:12 -0700

    bugs

--- Assets/Scripts/Spells/Projectile.cs
+++
@@ -1,8 +0,0 @@
-public class ProjectileData
-{
-    public string Trajectory;
-    public string SpeedExpr;
-    public string LifetimeExpr;
-    public int Sprite;
-    public string PierceCount;
-}
