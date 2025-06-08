a08d0692af81edd9f3167c074957d4fe70d6ae9b
Samantha Siew <sammie.siew@gmail.com>
2025-06-08 03:07:10 -0700

    better ui

---
+++ Assets/Scripts/Spells/SpellRewardOptionUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SpellRewardOptionUI : MonoBehaviour
+{
+    public SpellUI spellUI;
+    public TMP_Text spellDescription;
+    public Button acceptButton;
+
+    private Spell spellData;
+
+    public void Setup(Spell spell, System.Action<Spell> onAccept)
+    {
+        spellData = spell;
+        spellUI.SetSpell(spell, -1);
+
+        string definitionText = spell.GetFullName() + "\n";
+        foreach (var pair in spell.GetDefinitionList())
+        {
+            definitionText += $"{pair.Key}: {pair.Value}\n";
+        }
+        spellDescription.text = definitionText;
+
+        acceptButton.onClick.RemoveAllListeners();
+        acceptButton.onClick.AddListener(() => onAccept?.Invoke(spellData));
+    }
+}
